2f88aafc440ca6c33a0fba787f9de1ffe8e45bca
Wangke-git <1173750591@qq.com>
2020-02-15 13:53:38 +0800

    输出*

---
+++ 1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int i, j;
+	for (i = 1; i < 8; i++)
+	{
+		for (j = 0; j < (2 * i - 1); j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (i = 6; i > 0; i--)
+	{
+		for (j = (i * 2) - 1; j > 0; j--)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
