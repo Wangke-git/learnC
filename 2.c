366b35d06654f60924cc8e4e169562d3496bac09
Wangke-git <1173750591@qq.com>
2020-02-18 12:37:25 +0800

    替换

--- 2.c
+++ 2.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<Windows.h>
 int main()
 {
 	char str1[] = "welcome to new world";
@@ -13,8 +14,10 @@ int main()
 		str2[right] = str1[right];
 		left += 1;
 		right -= 1;
+		system("cls");
+		//cls:clear screen :cmd命令
 		printf("%s\n", str2);
-		Sleep();
+		Sleep(500);
 		//Sleep表示睡眠，程序放弃Cpu,休息一会。sleep为操作系统的函数
 	}
 
