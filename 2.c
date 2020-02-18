bf2571d3c4ab28e5e7952d48315af030d6dd2d92
Wangke-git <1173750591@qq.com>
2020-02-18 12:31:15 +0800

    逐渐交换俩数组的值，并一布一步显示出来

---
+++ 2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main()
+{
+	char str1[] = "welcome to new world";
+	char str2[] = "####################";
+	int left = 0;
+	int right = strlen(str1)-1;
+	while (left <= right)
+	{
+		str2[left] = str1[left];
+		str2[right] = str1[right];
+		left += 1;
+		right -= 1;
+		printf("%s\n", str2);
+		Sleep();
+		//Sleep表示睡眠，程序放弃Cpu,休息一会。sleep为操作系统的函数
+	}
+
+	system("pause");
+	return 0;
+}
