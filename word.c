c3fb498932ba8da8004ce9e9b8e08fd6346fc1f0
Wangke-git <1173750591@qq.com>
2020-02-20 12:57:58 +0800

    字母大小写转换

---
+++ word.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main() {
+	char ch;
+	while (1) {
+		printf("ÇëÊäÈëÒ»¸ö×Ö·û£º\n");
+		ch = getchar();
+		getchar();
+		if (ch >= 'a'&&ch <= 'z') {
+			printf("%c\n", ch - 32);
+		}
+		else if (ch >= 'A'&&ch <= 'Z') {
+			printf("%c\n", ch + 32);
+		}
+		else if (ch >= '0'&&ch <= '9') {
+			;
+		}
+	}
+	system("pause");
+	return 0;
+}
